f885e7fbbe216033422e44db435f58df3be2ef4e
jsonMartin <4033621+jsonMartin@users.noreply.github.com>
2023-07-28 12:51:28 -0700

    Add Dist folder

---
+++ dist/timeline/Group.svelte
@@ -0,0 +1,64 @@
+<script>import { twMerge } from 'tailwind-merge';
+export let divClass = 'p-5 mb-4 bg-gray-50 rounded-lg border border-gray-100 dark:bg-gray-800 dark:border-gray-700';
+export let timeClass = 'text-lg font-semibold text-gray-900 dark:text-white';
+export let date;
+export let olClass = 'mt-3 divide-y divider-gray-200 dark:divide-gray-700';
+let divCls = twMerge(divClass, $$props.classDiv);
+let timeCls = twMerge(timeClass, $$props.classTime);
+let olCls = twMerge(olClass, $$props.classOl);
+</script>
+
+<div class={divCls}>
+  <time class={timeCls}>{date}</time>
+  <ol class={olCls}>
+    <slot />
+  </ol>
+</div>
+
+<!--
+  @component
+  ## Features
+  [Go to Grouped Timeline](https://flowbite-svelte.com/docs/components/timeline#Grouped_timeline)
+  - Setup
+  - Default timeline
+  - Vertical Timeline
+  - Horizontal Timeline
+  - Activity Log
+  - Grouped timeline
+  ## Props
+  @prop divClass: string;
+  @prop timeClass: string;
+  @prop date: Date | string;
+  @prop olClass: string;
+  ## Example
+  ```
+  <script>
+    import { Group, GroupItem } from 'flowbite-svelte';
+    let groupTimelines = [
+      {
+        title:
+          '<span class="font-medium text-gray-900 dark:text-white">Jese Leos</span> likes <span class="font-medium text-gray-900 dark:text-white">Bonnie Green\'s</span> post in <span class="font-medium text-gray-900 dark:text-white"> How to start with Flowbite library</span>',
+        src: '/images/profile-picture-1.webp',
+        alt: 'alt here',
+        href: '/',
+        isPrivate: true,
+        comment: '"I wanted to share a webinar zeroheight."'
+      },
+      {
+        title:
+          '<span class="font-medium text-gray-900 dark:text-white">Bonnie Green</span> react to <span class="font-medium text-gray-900 dark:text-white">Thomas Lean\'s</span> comment',
+        src: '/images/profile-picture-2.webp',
+        alt: 'alt here',
+        href: '/',
+        isPrivate: true,
+        comment: '"I wanted to share a webinar zeroheight."'
+      }
+    ];
+  </script>
+
+  <Group date="January 13th, 2022">
+    <GroupItem timelines={groupTimelines} />
+  </Group>
+  ```
+  
+-->
---
+++ dist/typography/A.svelte
@@ -0,0 +1,42 @@
+<script>import { twMerge } from 'tailwind-merge';
+export let href = '#';
+export let color = 'text-primary-600 dark:text-primary-500';
+export let aClass = 'inline-flex items-center hover:underline';
+</script>
+
+<a {...$$restProps} {href} class={twMerge(aClass, color, $$props.class)} on:click>
+  <slot />
+</a>
+
+<!--
+  @component
+  ## Features
+  [Go to Link](https://flowbite-svelte.com/docs/typography/link)
+  - Setup
+  - Default link
+  - Button
+  - Paragraph
+  - Icon link
+  - CTA link
+  - Card link
+  - Image link
+  ## Props
+  @prop italic: boolean = false;
+  @prop underline: boolean = false;
+  @prop linethrough: boolean = false;
+  @prop uppercase: boolean = false;
+  @prop gradient: boolean = false;
+  @prop highlight: boolean = false;
+  @prop highlightClass: string = 'text-blue-600 dark:text-blue-500';
+  @prop decorationClass: string = 'decoration-2 decoration-blue-400 dark:decoration-blue-600';
+  @prop gradientClass: string =
+    'text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400';
+  ## Example
+  ```
+  <script>
+    import { A } from 'flowbite-svelte';
+  </script>
+
+  <A class="font-medium hover:underline">Read more</A>
+  ```
+-->
